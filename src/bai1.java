2b05c86008d23292d2db236d0ec9b6ec7ccedbcb
nghialm <nghialm@bap.jp>
2023-10-19 14:59:09 +0700

    create project java

---
+++ src/bai1.java
@@ -0,0 +1,7 @@
+public class bai1 {
+    public static void main(String[] args) {
+        System.out.println("dsjghfdhj");
+        System.out.println(21545);
+        System.out.print(454);
+    }
+}
---
+++ src/bai2.java
@@ -0,0 +1,8 @@
+public class bai2 {
+    public static void main(String[] args) {
+        System.out.println("dong1");
+        System.out.println("dong2");
+        System.out.print("dong3");
+        System.out.print("\tdong4");
+    }
+}
