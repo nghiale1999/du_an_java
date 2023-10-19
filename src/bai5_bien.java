7c2bfc12790277336dd0508fc971f2340b99ae66
nghialm <nghialm@bap.jp>
2023-10-19 15:39:43 +0700

    bai5 bien

---
+++ src/bai5_bien.java
@@ -0,0 +1,12 @@
+public class bai5_bien {
+    public static void main(String[] args) {
+        // khai báo
+        int a;
+        char b;
+        float c, d, e;
+
+        // khởi tạo
+        int tuoi = 10;
+        System.out.println(tuoi);
+    }
+}
